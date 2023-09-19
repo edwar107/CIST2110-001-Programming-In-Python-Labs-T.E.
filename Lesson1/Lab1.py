31b37e5ebda8fbbcf592aabb7ca06e5cc4893779
edwar107 <edwar107@go.stockton.edu>
2023-09-18 23:10:49 -0400

    completed Lab 1

--- Lesson1/Lab1.py
+++ Lesson1/Lab1.py
@@ -6,7 +6,7 @@
 # Note: to "uncomment a line", remove the # at the beginning of the line. The shortcut is to select the line and press Ctrl + / (windows) or Command + / (mac)
 # Note2: to run the program, Select the "Play" button at the top of the screen.
 
-print("Hello World!")
+print("Hello world") 
 
 # 2. Add a print statement that prints your name. Run the program and show the output to your instructor.
 
