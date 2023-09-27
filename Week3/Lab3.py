a674f1af2b9f5161d063b513a7b34ef8d1109843
edwar107 <edwar107@go.stockton.edu>
2023-09-27 13:12:25 -0400

    Commit Changes Lab 3 Corrected

--- Week3/Lab3.py
+++ Week3/Lab3.py
@@ -11,21 +11,21 @@ print(name)
 # 2. Ask the user for their age and assign it to a variable called "age".
 age = int(input("What is your age?"))
 # 3. Ask the user for a balance and assign it to a variable called "balance".
-balance = input("What is your balance?")
+balance = float(input("What is your balance?"))
 print(balance)
 # 4. Ask the user for a number of years to calculate interest and assign it to a variable called "years".
-years = input("What is the years?")
+years = int(input("What is the years?"))
 print(years)
 # 5. Ask the user for an interest rate and assign it to a variable called "interest_rate".
 interest_rate = input("what is the interest_rate?")
 print(interest_rate)
 # 6. Convert the balance,  years, and interest_rate to float data types.
-my_float_balance = float()
+
 # Note: This is only necessary if you did not specify the data type in the input() function.
 
 # 7. Calculate the future balance using the formula: balance * (1 + interest_rate/100) ** years
-balance = * (1 + interest_rate/100) * 22
+future_balance = (balance * (1 + interest_rate/100) ** 22)
 # 8. Round the future balance to 2 decimal places using the round() function.
-round = input(balance )
+round = round(future_balance, 2)
 # 9. Print the following sentence using string concatenation: "Hello <name>, your balance after <years> years will be $<future_balance>."
-print("Hello <name>, your balance after <years> years will be $<future_balance>.")
+print("Hello "+ name +", your balance after " + str(years) + " years will be" + int(future_balance)
